cfb486da783b22ff14f132dfba6dc4b6e83d8257
yahya-cade <yahyakade790@gmail.com>
2023-12-12 16:05:32 +0300

    setup EditCustomer which still doesn't work but DeleteCustomer works properly

--- src/Models/CustomerForms/DeleteCustomer.jsx
+++ src/Models/CustomerForms/DeleteCustomer.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "../../api/axiosConfig";
 
-export const DeleteCustomer = () => {
-  return <div>DeleteCustomer</div>;
+export const DeleteCustomer = ({ id, onClose }) => {
+  const [loading, setLoading] = useState(false);
+
+  const deleteCustomer = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.delete(`/api/v1/customers/${id}`);
+
+      if (res.status === 200) {
+        setLoading(false);
+        onClose();
+      }
+    } catch (error) {
+      alert(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    deleteCustomer();
+  }, [id]);
+  //   return (
+  //     <div className="flex items-center justify-center h-full">
+  //       {loading ? (
+  //         <p className="text-xl font-semibold">Deleting...</p>
+  //       ) : (
+  //         <p className="text-xl font-semibold">Deletion completed.</p>
+  //       )}
+  //     </div>
+  //   );
 };
--- src/Models/CustomerForms/EditCustomer.jsx
+++ src/Models/CustomerForms/EditCustomer.jsx
@@ -1,36 +1,91 @@
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import axios from "../../api/axiosConfig";
 import { IoCloseOutline } from "react-icons/io5";
 
 export const EditCustomer = ({ onClose }) => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [address, setAddress] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [agency_id, setAgency_id] = useState("");
+  const [agents, setAgents] = useState([]);
+
+  const handleClear = () => {
+    setName("");
+    setContact("");
+    setAddress("");
+    setAgency_id("");
+  };
+
   const schema = yup.object().shape({
-    id: yup.string().required(),
-    name: yup.string().required(),
-    business: yup.string().email().required(),
-    contact: yup.number().required(),
+    fullname: yup.string().required(),
+    phone: yup.string().required(),
+    address: yup.string().required(),
+    agency_id: yup.string().required(),
   });
 
   const { register, handleSubmit } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const fetchCustomer = () => {
+    const res = axios.get(`/api/v1/customers/${id}`);
+
+    if (res.status === 200) {
+      const agentData = res.data[0];
+      setFullName(agentData?.fullname || "");
+      setDescription(agentData?.description || "");
+      setBusiness(agentData?.business || "");
+      setContact(agentData?.phone || "");
+
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCustomer();
+  }, []);
+
+  useEffect(() => {
+    const fetchAgents = async () => {
+      const res = await axios.get(`/api/v1/agents`);
+
+      if (res.status === 200) {
+        const agentsData = await res.data;
+        setAgents(agentsData.data);
+      }
+    };
+
+    fetchAgents();
+  }, []);
+
+  const EditCustomer = async (data) => {
+    data.agency_id = agency_id;
+    setLoading(true);
+    const res = await axios.put(`/api/v1/customers`, data);
+
+    if (res.status === 201) {
+      alert("Customer Registered Successfully");
+      handleClear();
+      setLoading(false);
+    }
   };
 
   return (
     <div className="flex bg-slate-100">
       <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-1 bg-white shadow-slate-300 shadow-sm w-[38rem] h-[54rem] rounded-xl p-3"
+        onSubmit={handleSubmit(EditCustomer)}
+        className="flex flex-col gap-1 bg-white shadow-slate-300 shadow-sm w-[38rem] h-[38rem] rounded-lg p-3"
       >
         <div className="pb-16 ml-5 mt-8">
           <div className="flex">
-            <h2 className="text-3xl ml-40">Edit Customer</h2>
+            <h2 className="text-3xl ml-40">Add New Customer</h2>
             <button
               onClick={onClose}
-              className=" h-8 w-8 p-1 bg-blue-500 text-white text-2xl font-medium rounded-md hover:bg-blue-600 ml-36"
+              className="h-8 w-8 p-1 bg-blue-500 text-white text-2xl font-medium rounded-md hover:bg-blue-600 ml-28"
             >
               <IoCloseOutline />
             </button>
@@ -38,59 +93,60 @@ export const EditCustomer = ({ onClose }) => {
 
           <div className="space-y-6">
             <div className="flex flex-col gap-1">
-              <label>ID</label>
-              <input
-                type="text"
-                {...register("firstname")}
-                className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
-                placeholder="1"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
               <label>Name</label>
               <input
                 type="text"
-                {...register("lastname")}
+                {...register("fullname")}
                 className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
                 placeholder="john"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
-
             <div className="flex flex-col gap-1">
-              <label>Contact </label>
+              <label>Contact</label>
               <input
-                type="number"
+                type="string"
                 {...register("phone")}
                 className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
-                placeholder="123456789"
+                placeholder="123456"
+                value={contact}
+                onChange={(e) => setContact(e.target.value)}
               />
             </div>
             <div>
               <label>Address </label>
               <input
-                type="number"
+                type="text"
                 {...register("address")}
                 className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
                 placeholder="mogadishu..."
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
               />
             </div>
+
             <div>
-              <label>Description</label>
-              <input
-                type="number"
-                {...register("description")}
-                className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
-                placeholder="simething..."
-              />
-            </div>
-            <div>
-              <label>Agency</label>
-              <input
-                type="text"
-                {...register("phone")}
-                className=" bg-[#F9F9F9] placeholder:text-slate-400 p-3 mr-1 rounded-lg w-[34rem]"
-                placeholder="web..."
-              />
+              <label>Agent</label>
+              <select
+                {...register("agency_id")}
+                defaultValue={agency_id}
+                onChange={(e) => setAgency_id(e.target.value)}
+                className="placeholder:text-slate-700 p-3 mr-1 rounded-lg w-[34rem]"
+              >
+                <option value="" disabled>
+                  Select an agent
+                </option>
+                {agents.map((agent) => (
+                  <option
+                    key={agent.id}
+                    value={agent.id}
+                    className="text-black bg-white"
+                  >
+                    {agent.name}
+                  </option>
+                ))}
+              </select>
             </div>
           </div>
 
@@ -103,7 +159,7 @@ export const EditCustomer = ({ onClose }) => {
               type="submit"
               className="p-1 mr-1 rounded-lg w-28 h-12 mt-10 bg-blue-600 text-white text-xl "
             >
-              Edit
+              Submit
             </button>
             <button
               type="submit"
